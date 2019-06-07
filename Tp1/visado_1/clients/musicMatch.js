d2ca9fc5bd68b13efeb3ea17a078870d1c63cf81
Dova81 <33400774+Dova81@users.noreply.github.com>
2019-06-07 00:59:50 -0300

    cliente de musicmatch y api key creados

--- Tp1/visado_1/clients/musicMatch.js
+++ Tp1/visado_1/clients/musicMatch.js
@@ -1,9 +1,45 @@
-const request = require('request-promise');
+const rp = require('request-promise');
+const cred = require('../credentials/musicMatchCreds.json')
 
-module.exports = () => {
-  const options = {
-    url: '',
-    headers: {}
-  };
-  return request(options);
+
+class MusicMatchClient {
+
+  constructor() {
+      this.apiKey = cred.apiKey
+      this.baseURL = 'http://api.musixmatch.com/ws/1.1'
+  }
+
+
+  searchTrackId(trackName) {
+    const options = {
+      uri: this.baseURL + '/track.search',
+      qs: {
+      apikey: this.apiKey,
+      q_track: trackName,
+      },
+      json: true
+    };
+        
+        
+        return rp.get(options).then((response) =>response.message.body.track_list[0].track.track_id).catch(error =>console.log(error));
+  }
+
+  getTrackLyrics(trackID) {
+    const options = {
+      uri: this.baseURL + '/track.lyrics.get',
+      qs: {
+      apikey: this.apiKey,
+      track_id: trackID,
+      },
+      json: true
+    }
+      return rp.get(options).then((response) =>response.message.body.lyrics.lyrics_body).catch(error =>console.log(error));
+  }
+
+
+
+}
+module.exports = {
+  MusicMatchClient,
 };
+
