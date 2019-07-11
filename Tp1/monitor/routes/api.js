75f7fa3b65697891d5cc4f1db305cae2039a317c
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-07-11 18:14:43 -0300

    interval

--- Tp1/monitor/routes/api.js
+++ Tp1/monitor/routes/api.js
@@ -4,7 +4,17 @@ const express = require('express');
 
 const api = express.Router();
 
-api.get('/artists', (req, res, next) => {
+const monitoring = () => setInterval(() => console.log('AAAAAAAAAAA'), 1500);
+
+let interval;
+
+api.post('/set', (req, res, next) => {
+  interval = monitoring();
+  return res.status(200).send({});
+});
+
+api.post('/clear', (req, res, next) => {
+  clearInterval(interval);
   return res.status(200).send({});
 });
 
