1838d76ace08f4f6e8f6e11d658e63ba8de385cb
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-07-11 21:01:03 -0300

    repos

--- Tp1/logs/app.js
+++ Tp1/logs/app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const api = require('./routes/api');
 const { handleError, notFoundError } = require('./middlewares/errors');
-const { unqfyResponse } = require('./config/unqfy');
 
 const app = express();
 
@@ -13,7 +12,6 @@ app.use(bodyParser.urlencoded({ extended: false, useNewUrlParser: true }));
 
 app.use('/api', api);
 app.use('/*', notFoundError);
-app.use(unqfyResponse);
 app.use(handleError);
 
 module.exports = app;
