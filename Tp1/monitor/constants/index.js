f24efadca73bcb99aba36b8133f154b1ec7a12a2
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-07-13 23:45:16 -0300

    try docker

--- Tp1/monitor/constants/index.js
+++ Tp1/monitor/constants/index.js
@@ -1,3 +1,3 @@
-exports.logsUrl = 'http://localhost:8003/api/active';
-exports.notifyUrl = 'http://localhost:8001/api/active';
+exports.logsUrl = process.env.LOGS_PORT ? `${process.env.LOGS_PORT}/api/active` : 'localhost:8003/api/active';
+exports.notifyUrl = process.env.NOTIFY_PORT ? `${process.env.NOTIFY_PORT}/api/active` : 'localhost:8001/api/active';
 exports.slackUrl = 'https://hooks.slack.com/services/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';
