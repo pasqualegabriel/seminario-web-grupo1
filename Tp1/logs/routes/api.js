87d50769028e8d566ca76719ed5d3606557e4aee
Mauro <reekremag@gmail.com>
2019-07-15 00:00:07 -0300

    merge con master

--- Tp1/logs/routes/api.js
+++ Tp1/logs/routes/api.js
@@ -8,6 +8,7 @@ const api = express.Router();
 api.post('/activate', logger.activate);
 api.post('/desactivate', logger.desactivate);
 api.post('/save', logger.save);
+api.get('/active', (req, res, next) => res.status(200).send('ok'));
 
 module.exports = api;
 
