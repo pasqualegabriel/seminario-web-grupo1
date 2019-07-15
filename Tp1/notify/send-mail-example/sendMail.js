7fc958c0a8065e5579d937cc982d2dd118eebbc7
Dova81 <33400774+Dova81@users.noreply.github.com>
2019-07-14 23:21:41 -0300

    asdfasfasdfasd

--- Tp1/notify/send-mail-example/sendMail.js
+++ Tp1/notify/send-mail-example/sendMail.js
@@ -7,31 +7,34 @@ const getGmailClient = require('./gmailClient');
 // Obtiene un objeto JJJJJ a partir del credentials.json y token.json
 const gmailClient = getGmailClient();
 
-// Envia un mail, utilizando la funcion ZZZZ que termina haciendo un request a XXXXX
-gmailClient.users.messages.send(
-  {
-    userId: 'me',
-    requestBody: {
-      raw: createMessage(),
-    },
-  }
-);
+function notificarSuscriptor(listaMail,nombreArtista,año,nombreAlbum){
 
+  listaMail.forEach(m => {
+    gmailClient.users.messages.send(
+      {
+        userId: 'me',
+        requestBody: {
+          raw: createMessage(m,nombreArtista, año,nombreAlbum),
+        },
+      }
+    );  
+  });
+  
+}
 
-function createMessage() {
+function createMessage(mail,nombreArtista, año,nombreAlbum) {
     // You can use UTF-8 encoding for the subject using the method below.
     // You can also just use a plain string if you don't need anything fancy.
     const subject = 'Hello';
     const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString('base64')}?=`;
     const messageParts = [
       'From: Lautaro Woites <testCloudUnq@gmail.com>',
-      'To: Lautaro Woites <testCloudUnq@gmail.com>',
+      `To: Lautaro Woites ${mail}`,
       'Content-Type: text/html; charset=utf-8',
       'MIME-Version: 1.0',
       `Subject: ${utf8Subject}`,
-      '',
-      'This is a message just to say hello.',
-      'So... <b>Hello!</b> ❤️😎',
+      `El artista ${nombreArtista} acaba de sacar el album ${nombreAlbum}`,
+    
     ];
     const message = messageParts.join('\n');
   
