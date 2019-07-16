37480838429878f464b691de57356ed2e6f46005
f.autalan <f.n.autalan@gmail.com>
2019-07-15 21:41:23 -0300

    add

--- Tp1/visado_1/controllers/suscriptorController.js
+++ Tp1/visado_1/controllers/suscriptorController.js
@@ -1,24 +1,22 @@
 exports.suscribe = (req, res, next) => {
-    console.log('llegue');
-    
-    res.locals.unqfy.suscribe(parseInt(req.body.artistId),req.body.email);
-    return next({ status: 200, data: {}});
+  console.log('llegue');
+
+  res.locals.unqfy.suscribe(parseInt(req.body.artistId), req.body.email);
+  return next({ status: 200, data: {} });
 };
 
 exports.unsubscribe = (req, res, next) => {
+  const artista = res.locals.unqfy.unsubscribe(parseInt(req.body.artistId), req.body.email);
 
-    const artista = res.locals.unqfy.unsubscribe(parseInt(req.body.artistId),req.body.email);
-
-    return next({ status: 200, data: {artista}});
+  return next({ status: 200, data: { artista } });
 };
 
-exports.subscriptors = (req,res,next) => {
-    
-    const artistId = parseInt(req.params.artitsId);
-    const subscriptors = res.locals.unqfy.subscriptors(artistId);
-    const datos = {
-        'artistId': artistId,
-        'subscriptors': subscriptors
-    }
-    return next({ status: 200, data: datos});
+exports.subscriptors = (req, res, next) => {
+  const artistId = parseInt(req.params.artitsId);
+  const subscriptors = res.locals.unqfy.subscriptors(artistId);
+  const datos = {
+    artistId,
+    subscriptors
+  };
+  return next({ status: 200, data: datos });
 };
