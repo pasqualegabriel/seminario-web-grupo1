431bcdef7f4202da51b305195ac1c53ddb284d4b
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-07-16 00:48:50 -0300

    script added

--- Tp1/visado_1/crearEntorno.sh
+++ Tp1/visado_1/crearEntorno.sh
@@ -9,8 +9,10 @@ node main.js addArtist PabloLescano Argentina
 node main.js addUsuario pasquboca12@gmail.com
 node main.js addUsuario n.autalan@gmail.com 
 node main.js addUsuario reekremag@gmail.com
-
-
+node main.js subscribe 1 pasquboca12@gmail.com
+node main.js subscribe 1 n.autalan@gmail.com 
+node main.js subscribe 1 reekremag@gmail.com
+node main.js addAlbum 1 ATRCUMBIAPIOLA 2018
 echo
 echo "------------------------------"
 echo
